af503949f52e4c6a31c2b31fb3c1f9bcad83745a
DESKTOP-RRPRPE7\Lenovo <yz1410aa@gmail.com>
2023-05-15 20:10:12 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import GuessNum from "./components/GuessNum.vue";
+export default {
+   components: {
+      GuessNum,
+   },
+};
+</script>
+
+<template>
+   <div class="guess-area">
+      <GuessNum></GuessNum>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.guess-area {
+   width: 100vw;
+   height: 100vh;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+</style>
---
+++ src/components/GuessNum.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+   data() {
+      return {
+         lowNum: 1,
+         highNum: 100,
+         guessNum: null,
+         ans: Math.floor(Math.random() * 100) + 1,
+
+         trigger: 1,
+      };
+   },
+   methods: {
+      checkNum() {
+         this.trigger = 1;
+         if (this.guessNum === this.ans) {
+            this.trigger = 0;
+         } else if (this.guessNum < this.ans && this.guessNum > this.lowNum) {
+            this.lowNum = this.guessNum;
+         } else if (this.guessNum > this.ans && this.guessNum < this.highNum) {
+            this.highNum = this.guessNum;
+         } else {
+            this.trigger = 2;
+         }
+         this.guessNum = null;
+      },
+      checkScope() {
+         if (this.trigger == 1 || this.trigger == 2) {
+            return true;
+         }
+         return false;
+      },
+   },
+   mounted() {},
+};
+</script>
+
+<template>
+   <div class="card-area">
+      <h1 v-if="trigger == 1">猜數字</h1>
+      <h1 v-if="trigger == 0">恭喜答對了</h1>
+      <h1 v-if="trigger == 2">請猜範圍內的數字</h1>
+      <div class="num-area d-flex">
+         <div class="check-area d-flex" v-if="checkScope()">
+            <p>{{ lowNum }}</p>
+            <p>-</p>
+            <p>{{ highNum }}</p>
+         </div>
+
+         <p v-if="trigger == 0">{{ ans }}</p>
+      </div>
+
+      <input type="number" v-model="guessNum" />
+      <button type="button" @click="checkNum()">回答</button>
+   </div>
+</template>
+
+<style lang="scss" scoped>
+.card-area {
+   width: 40vw;
+   height: 80vh;
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   .num-area {
+      p {
+         margin: 1rem 1rem;
+      }
+   }
+   button {
+      margin-top: 1rem;
+   }
+}
+</style>
